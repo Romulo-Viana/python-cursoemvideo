b3a6d706efa2788fb0df07694c57f39a6a1bf1aa
Romulo-Viana <romulovianalast@gmail.com>
2023-09-26 09:33:50 -0300

    estudando módulos

---
+++ desafios/d016.py
@@ -0,0 +1,4 @@
+from math import trunc
+n = float(input('Digite um valor: '))
+inteiro = trunc(n)
+print(f'O valor inteiro é: {inteiro}')
---
+++ desafios/d019.py
@@ -0,0 +1,4 @@
+import random
+alunos = random.random('')
+
+print(f'O aluno sorteado para apagar o quadro é {alunos}')
